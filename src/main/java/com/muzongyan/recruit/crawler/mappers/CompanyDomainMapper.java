a62903b9c970455eed36b38bb7243ffbd2e8569f
muzongyan <muzongyan@163.com>
2014-12-29 09:28:14 +0800

    更新发布

--- src/main/java/com/muzongyan/recruit/crawler/mappers/CompanyDomainMapper.java
+++ src/main/java/com/muzongyan/recruit/crawler/mappers/CompanyDomainMapper.java
@@ -18,13 +18,13 @@ import com.muzongyan.recruit.crawler.dtos.CompanyDomain;
  */
 public interface CompanyDomainMapper {
 
+    public void addAll(List<CompanyDomain> list);
+    
     @Insert("INSERT INTO lagou_company_domain(name, url, create_time)"
             + " VALUES (#{domain.name}, #{domain.url}, now())"
             + " ON DUPLICATE KEY UPDATE name = VALUES(name), update_time = now()")
     public void add(@Param("domain") CompanyDomain domain);
     
-    public void addAll(List<CompanyDomain> list);
-
     @Select("SELECT url FROM lagou_company_domain")
     public Set<String> getAllUrl();
     
